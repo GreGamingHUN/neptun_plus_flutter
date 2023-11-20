f19d2ccf3d70e947209a56d084f4700baffb6183
Práger Gergő <prager.gergo@gmail.com>
2023-11-20 12:47:10 +0100

    added subjects screen

---
+++ lib/widgets/added_subjects_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddedSubjectsScreen extends StatelessWidget {
+  const AddedSubjectsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
