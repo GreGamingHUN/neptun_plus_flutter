1f0ccf4d17018c925655d73b04f7242595ac1ae7
Práger Gergő <prager.gergo@gmail.com>
2023-12-10 13:13:48 +0100

    date formatting

--- lib/logic.dart
+++ lib/logic.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 String trimString(String input, int length) {
   return (input.length > length ? "${input.substring(0, length)}..." : input);
 }
@@ -8,4 +10,20 @@ String trimCSSfromMessage(String message) {
     return message.substring(message.lastIndexOf(cssPostfix) + 12);
   }
   return message;
+}
+
+String formatDate(String date) {
+  RegExp regExp = RegExp(r'/Date\((\d+)\)/');
+  Match? match = regExp.firstMatch(date);
+
+  if (match != null) {
+    int dateNumber = int.parse(match.group(1)!);
+    DateTime sendTime = DateTime.fromMillisecondsSinceEpoch(dateNumber);
+    DateTime currentTime = DateTime.now();
+    if (sendTime.compareTo(currentTime.subtract(const Duration(days: 1))) < 0) {
+      return DateFormat('MM. dd.').format(sendTime);
+    }
+    return DateFormat('HH:mm').format(sendTime);
+  }
+  return 'NaN';
 }
