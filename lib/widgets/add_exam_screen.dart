e25381dc08878ba979c9ca72174d5ddca3e7233b
GreGamingHUN <prager.gergo@gmail.com>
2024-02-12 22:33:08 +0100

    fix building errors

--- lib/widgets/add_exam_screen.dart
+++ lib/widgets/add_exam_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:neptun_plus_flutter/widgets/exam_details_screen.dart';
 import 'api_calls.dart' as api_calls;
