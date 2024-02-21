fa6153f0632cf647180459fb679f66373adad46d
GreGamingHUN <prager.gergo@gmail.com>
2024-02-21 15:12:05 +0100

    feat: updater markdown support

--- lib/widgets/dialogs/update_dialog.dart
+++ lib/widgets/dialogs/update_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:neptun_plus_flutter/src/updater.dart';
 
 // ignore: must_be_immutable
@@ -7,27 +8,46 @@ class UpdateDialog extends StatelessWidget {
   String changelog;
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Új frissítés érhető el!', style: TextStyle(fontWeight: FontWeight.w600),),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text('Egy új verzió érhető el, kérlek frissíts, hogy megkapd a legújabb feautre-öket és bugfixeket!'),
-          const Divider(),
-          const Padding(
-            padding: EdgeInsets.only(bottom: 16.0),
-            child: Text('Mi változott:', style: TextStyle(fontSize: 24),),
-          ),
-          Text(changelog)
-        ],
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                'Új frissítés érhető el!',
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: Theme.of(context).textTheme.titleLarge!.fontSize),
+              ),
+            ),
+            const Text(
+                'Egy új verzió érhető el, kérlek frissíts, hogy megkapd a legújabb feautre-öket és bugfixeket!'),
+            const Divider(),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Mi változott:',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            MarkdownBody(data: changelog,shrinkWrap: true, styleSheet: MarkdownStyleSheet(blockSpacing: 0)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: () => Navigator.pop(context), child: const Text('Később')),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: FilledButton(onPressed: () => downloadUpdate(), child: const Text('Frissítés')),
+                )
+              ],
+            )
+          ],
+        ),
       ),
-      actions: [
-        TextButton(onPressed: () => Navigator.pop(context), child: const Text('Később')),
-        FilledButton(onPressed: () {
-          downloadUpdate();
-        }, child: const Text('Frissítés'))
-      ],
     );
   }
 }
